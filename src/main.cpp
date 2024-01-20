d3c288232d4b944db60b7ec108ee791e91a3fa52
Shang Liang <contact@shang-liang.com>
2024-01-20 08:57:48 +0800

    Finished benchmarking 3 boards with Hello world example

--- src/main.cpp
+++ src/main.cpp
@@ -1,112 +1,66 @@
+#include <zephyr/kernel.h>
 #include <tensorflow/lite/micro/micro_mutable_op_resolver.h>
-#include <tensorflow/lite/micro/micro_log.h>
 #include <tensorflow/lite/micro/micro_interpreter.h>
 #include <tensorflow/lite/micro/system_setup.h>
 #include <tensorflow/lite/schema/schema_generated.h>
-
-#include "model_mnist.h" /* quantized model */
-#include "images.h"
+#include "model.h"
 
 namespace
 {
   const tflite::Model *model = nullptr;
   tflite::MicroInterpreter *interpreter = nullptr;
   TfLiteTensor *input = nullptr;
-  TfLiteTensor *output = nullptr;
-  int input_length;
-
-  /* Create an area of memory to use for input, output, and intermediate arrays.
-   * The size of this will depend on the model you're using, and may need to be
-   * determined by experimentation.
-   */
-  constexpr int kTensorArenaSize = 30 * 1024;
-  uint8_t tensor_arena[kTensorArenaSize];
+  // Output is ignored
+  // TfLiteTensor *output = nullptr;
+  // Not the smallest size it can be (760) but it does not need to be that small
+  constexpr int kTensorArenaSize = 2 * 1024;
+  static uint8_t tensor_arena[kTensorArenaSize];
 } /* namespace */
 
 int main(void)
 {
-
   // Map the model into a usable data structure..
-  model = tflite::GetModel(model_tflite);
-  if (model->version() != TFLITE_SCHEMA_VERSION)
-  {
-    MicroPrintf("Model schema version mismatch %d:%d", model->version(), TFLITE_SCHEMA_VERSION);
-    return -1;
-  }
-  else
-  {
-    MicroPrintf("Model version: %d", model->version());
-  }
-
-  // This pulls in all the operation implementations we need.
-  static tflite::MicroMutableOpResolver<6> resolver;
-  resolver.AddConv2D();
-  resolver.AddRelu();
-  resolver.AddMaxPool2D();
-  resolver.AddReshape();
+  model = tflite::GetModel(g_model);
+  // Add operation used in the model
+  static tflite::MicroMutableOpResolver<1> resolver;
   resolver.AddFullyConnected();
-  resolver.AddSoftmax();
-  MicroPrintf("resolver constructed");
+
   // Build an interpreter to run the model with.
   static tflite::MicroInterpreter static_interpreter(
       model, resolver, tensor_arena, kTensorArenaSize);
   interpreter = &static_interpreter;
-  MicroPrintf("Interpreter constructed");
 
   // Allocate memory from the tensor_arena for the model's tensors.
-  TfLiteStatus allocate_status = interpreter->AllocateTensors();
-  if (allocate_status != kTfLiteOk)
-  {
-    MicroPrintf("AllocateTensors() failed");
-    return -1;
-  }
-  else
-  {
-    MicroPrintf("AllocateTensor() Success");
-  }
+  TF_LITE_ENSURE_STATUS(interpreter->AllocateTensors());
 
-  size_t used_size = interpreter->arena_used_bytes();
-  MicroPrintf("Area used bytes: %d", used_size);
+  // Find out the exact tensor size
+  // printf("Area used bytes: %d\n", interpreter->arena_used_bytes());
   input = interpreter->input(0);
-  output = interpreter->output(0);
+  // Output is ignored
+  // output = interpreter->output(0);
 
-  MicroPrintf("Model input:");
-  MicroPrintf("input->type: %s", input->type);
-  MicroPrintf("dims->size: %s", input->dims->size);
-  for (int n = 0; n < input->dims->size; ++n)
+  float TWO_PI = 3.14159265359f * 2;
+  int NUM_CYCLES = 1000000;
+  int BATCH_SIZE = 1000;
+  printf("Running inference for %d cycles, %d a batch\n", NUM_CYCLES, BATCH_SIZE);
+  uint32_t start_time = k_uptime_get_32();
+  uint32_t batch_start_time = start_time;
+  for (int i = 0; i < NUM_CYCLES; i++)
   {
-    MicroPrintf("dims->data[n]: %d", input->dims->data[n]);
+    float x = TWO_PI * (i % BATCH_SIZE) / BATCH_SIZE;
+    /* Quantize the input from floating-point to integer */
+    int8_t x_quantized = x / input->params.scale + input->params.zero_point;
+    /* Place the quantized input in the model's input tensor */
+    input->data.int8[0] = x_quantized;
+    TF_LITE_ENSURE_STATUS(interpreter->Invoke());
+    if (i % BATCH_SIZE == 0 && i != 0)
+    {
+      uint32_t t = k_uptime_get_32();
+      printf("%04d, %d\n", i / BATCH_SIZE, t - batch_start_time);
+      batch_start_time = t;
+    }
   }
+  printf("Average time: %d\n", (k_uptime_get_32() - start_time) * BATCH_SIZE / NUM_CYCLES);
 
-  MicroPrintf("Model output:");
-  MicroPrintf("dims->size: %d", output->dims->size);
-  for (int n = 0; n < output->dims->size; ++n)
-  {
-    MicroPrintf("dims->data[n]: %d", output->dims->data[n]);
-  }
-
-  int width = 28, height = 28;
-
-  for (int i = 0; i < width * height; ++i)
-  {
-    input->data.f[i] = (float)(image[i] / 255.0);
-  }
-
-  MicroPrintf("Do inference");
-  // uint32_t start_time = k_uptime_get_32();
-  TfLiteStatus invoke_status = interpreter->Invoke();
-  if (invoke_status != kTfLiteOk)
-  {
-    MicroPrintf("Invoke failed");
-    return -1;
-  }
-  // uint32_t duration = k_uptime_get_32() - start_time;
-  // MicroPrintf("Inference time = %l", duration);
-
-  for (int n = 0; n < 10; ++n)
-  {
-    float value = output->data.f[n];
-    MicroPrintf("%n %f", n, value);
-  }
   return 0;
 }
--- train/convert_image.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-def convert(filename):
-	im=Image.open(filename).convert('L')
-	pixels = list(im.getdata()) 
-	print(pixels)
-
-
-convert("0001.bmp")
